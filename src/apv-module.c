f36e18f8fb967318afdee06d58cdfc531a34f07c
Samuele Valperta <samuele.valperta@gmail.com>
2024-09-27 12:23:20 +0200

    Lighten the syntax and fix some bugs.

--- src/apv-module.c
+++ src/apv-module.c
@@ -14,55 +14,49 @@
 #define EPS 1
 
 #define VAR_NUMBER 3
-#define dadt(a, p) ((ALFA * a) - EPS * p);
+#define dadt(a, p) ((ALFA * a) - (EPS * p * a));
 #define dpdt(a, p, v) ((THETA * p * a) - (MU * p) - (OMEGA * p * v))
 #define dvdt(p, v) ((BETA * p * v) - (GAMMA * v))
 
-void f(double a, double p, double v, double *results) {
-  *results = dadt(a, p);
-  *(results + 1) = dpdt(a, p, v);
-  *(results + 2) = dvdt(p, v);
+void f(double a, double p, double v, double increments[]) {
+  increments[0] = dadt(a, p);
+  increments[1] = dpdt(a, p, v);
+  increments[2] = dvdt(p, v);
 }
 
-void next(double *x, double h) {
-  double increments[] = {0, 0, 0};
-  f(*(x), *(x + 1), *(x + 2), increments);
+double *next(double *x, double h) {
+  double increments[VAR_NUMBER];
+  f(x[0], x[1], x[2], increments);
   for (int i = 0; i < VAR_NUMBER; i++) {
-    *((x + i) + VAR_NUMBER) = *(x + i) + h * (*(increments + i));
+    x[VAR_NUMBER + i] = fmax(0, x[i] + (h * increments[i]));
   }
+  return x + VAR_NUMBER;
 }
 
 static PyObject *getAPVMatrix(PyObject *Py_UNUSED(self), PyObject *args) {
-  double tf, t0, h, a_zero, p_zero, v_zero;
+  double ti, tf, h, a_zero, p_zero, v_zero;
 
-  if (!PyArg_ParseTuple(args, "dddddd", &tf, &t0, &h, &a_zero, &p_zero,
+  if (!PyArg_ParseTuple(args, "dddddd", &ti, &tf, &h, &a_zero, &p_zero,
                         &v_zero)) // "dddddd" beacuse we are expecting 6 doubles
     return NULL;
 
-  int n_steps = (int)floor((tf - t0) / h);
-  npy_intp dims[2] = {n_steps + 2, 3}; // + 2 beacuse of t0 and tf
+  unsigned int n_steps = (unsigned int)(tf - ti) / h;
+  npy_intp dims[2] = {n_steps, 3};
 
   PyObject *numpy_array = PyArray_SimpleNew(2, dims, NPY_FLOAT64);
   if (numpy_array == NULL) {
     return NULL;
   }
 
-  // double *x = (double *)malloc((n_steps)*VAR_NUMBER * sizeof(double));
-  // if (x == NULL) {
-  //   return NULL;
-  // }
-
   double *x = (double *)PyArray_DATA((PyArrayObject *)numpy_array);
 
-  *x++ = a_zero;
-  *x++ = p_zero;
-  *x++ = v_zero;
+  x[0] = a_zero;
+  x[1] = p_zero;
+  x[2] = v_zero;
 
-  for (int i = 0; i < n_steps; i++) {
-    next(x, h);
+  for (unsigned int i = 0; i < n_steps; i++) {
+    x = next(x, h);
   }
-
-  next(x, tf - (t0 + h * n_steps));
 
   return numpy_array;
 }
@@ -80,9 +74,6 @@ static struct PyModuleDef module_def = {
 
 PyMODINIT_FUNC PyInit_APV() {
   printf("Initialization\n");
+  import_array();
   return PyModule_Create(&module_def);
 }
-
-// PyMODINIT_FUNC initfoo(void) {
-//   import_array(); // enable NumPy C API
-// }
